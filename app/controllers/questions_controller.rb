4bf5eea1aa961f42faf14f20c3e5abd0adf3f5b5
Romee Weltevreden <romeeweltevreden@hotmail.com>
2024-02-19 14:45:36 +0100

    added code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,23 @@
 class QuestionsController < ApplicationController
+
+  # The controller will need to read the question from params
+  # and compute an instance variable @answer for the view to display.
+
+  def ask
+    @question = params[:ask]
+    @question
+  end
+
+  def answer
+    @question = ask()
+
+    if @question == "I am going to work"
+      @answer = "Great"
+    elsif @question.end_with? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
 end
